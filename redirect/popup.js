98da922022c7a5d1b4200a4db9e7379716de310b
Jeff Garnier <jeff1731@stanford.edu>
2016-01-17 16:18:37 -0800

    Not sure

--- redirect/popup.js
+++ redirect/popup.js
@@ -1,3 +1,52 @@
+redirectCurrentTab = function(Url){
+  chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
+        chrome.tabs.update(tab.id, {url: Url});
+    });
+}
+
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  return {
+    'total': t,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date(Date.parse(new Date()) + 60 * 60 * 1000);
+
+
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
   var checkPageButton = document.getElementById('checkPage');
   var changePageButton = document.getElementById('changePage');
@@ -9,79 +58,23 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('url').innerHTML = url;
 
     });
-
-    // chrome.tabs.getSelected(null, function(tab) {
-    //   d = document;
-
-    //   var f = d.createElement('form');
-    //   f.action = 'http://gtmetrix.com/analyze.html?bm';
-    //   f.method = 'post';
-    //   var i = d.createElement('input');
-    //   i.type = 'hidden';
-    //   i.name = 'url';
-    //   i.value = tab.url;
-    //   f.appendChild(i);
-    //   d.body.appendChild(f);
-    //   f.submit();
-    // });
   }, false);
 
   changePageButton.addEventListener('click', function() {    
-    window.open('http://www.google.com');
+    redirectCurrentTab("http://www.google.com")
   }, false);
 
 
   /////////
 
-  function getTimeRemaining(endtime) {
-    var t = Date.parse(endtime) - Date.parse(new Date());
-    var seconds = Math.floor((t / 1000) % 60);
-    var minutes = Math.floor((t / 1000 / 60) % 60);
-    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    var days = Math.floor(t / (1000 * 60 * 60 * 24));
-    return {
-      'total': t,
-      'days': days,
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
-    };
-  }
-
-  function initializeClock(id, endtime) {
-    var clock = document.getElementById(id);
-    var daysSpan = clock.querySelector('.days');
-    var hoursSpan = clock.querySelector('.hours');
-    var minutesSpan = clock.querySelector('.minutes');
-    var secondsSpan = clock.querySelector('.seconds');
-
-    function updateClock() {
-      var t = getTimeRemaining(endtime);
-
-      daysSpan.innerHTML = t.days;
-      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-      if (t.total <= 0) {
-        clearInterval(timeinterval);
-      }
-    }
-
-    updateClock();
-    var timeinterval = setInterval(updateClock, 1000);
-  }
-
-  var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-  initializeClock('clockdiv', deadline);
 
   //////
 
-
-
-
-
 }, false);
+
+
+
+
 
 
 
