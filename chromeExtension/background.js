2eac4300901a1a8b76ea9b8ba34bb45eaa3e1f9a
jamesross <jross3@stanford.edu>
2016-01-27 21:44:36 -0800

    changed xhr's so that each time a new site is visited, the xhr sends back 2 things: 1. the domain name of the new site 2. the time when that new site was first reached.

--- chromeExtension/background.js
+++ chromeExtension/background.js
@@ -1,32 +1,32 @@
-var d = new Date();
-var startTime = d.getTime();
-var prevUrl = "";
-var currUrl = "";
+
+function extractDomain(url) {
+    var domain;
+    if (url.indexOf("://") > -1) {
+        domain = url.split('/')[2];
+    }
+    else {
+        domain = url.split('/')[0];
+    }
+    domain = domain.split(':')[0];
+    return domain;
+}
 
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    currUrl = tabs[0].url;
-    prevUrl = currUrl;
-    console.log("starting url: " + currUrl);
+    console.log("starting domain: " + extractDomain(tabs[0].url));
 });
 
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.create({'url': "localhost:3000/users/index", 'selected': true});
 });
 
-var delay = 0;
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-    //console.log("In listener function callback");
-    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-        var tmpUrl = currUrl;
-        var dTmp = new Date();
-        amountSpent = (dTmp.getTime() - startTime - delay)/(1000);
-        var xhr = new XMLHttpRequest();
 
-        frontEndLogMessage = "Spent " + amountSpent.toString() + " seconds on " + currUrl;
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        var xhr = new XMLHttpRequest();
         xhr.addEventListener('readystatechange', function(evt) {
             if (xhr.readyState === 4) {
                 if (xhr.status === 200) {
-                    console.log(frontEndLogMessage);
+                    console.log("yay");
                 }
                 else {
                     console.log("ERROR: status " + xhr.status);
@@ -35,50 +35,29 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
         });
         xhr.open('POST', 'http://localhost:3000/users/record',true);
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        var form = 'url=' + tmpUrl + '&startTime=' + startTime.toString() + '&endTime=' + dTmp.getTime().toString();
+        var dTmp = new Date();
+        var form = 'url=' + extractDomain(tabs[0].url) + '&startTime=' + dTmp.toString();
         xhr.send(form);
-        //console.log("request sent: " + tmpUrl);
-        prevUrl = currUrl;
-        currUrl = tabs[0].url;
-        startTime = dTmp.getTime();
     });
 });
 
 
 chrome.tabs.onUpdated.addListener(function(tabid, changeInfo, tab) {
-    if(prevUrl !== currUrl) {
-        var url = tab.url;
-        var dTmp = new Date();
-        if (url !== undefined && changeInfo.status == "complete") {
-            amountSpent = (dTmp.getTime() - startTime - delay)/1000;
-            var tmpUrl = currUrl;
-            var xhr = new XMLHttpRequest();
-
-            var frontEndLogMessage = "Spent " + amountSpent.toString() + " seconds on " + tmpUrl + ". This made it to the backend.";
-                xhr.addEventListener('readystatechange', function (evt) {
-                if (xhr.readyState === 4) {
-                    if (xhr.status === 200) {
-                        console.log(frontEndLogMessage);
-                    } else {
-                        console.log("ERROR: status " + xhttp.status);
-                    }
+    if (tab.url !== undefined && changeInfo.status == "complete") {
+        var xhr = new XMLHttpRequest();
+        xhr.addEventListener('readystatechange', function (evt) {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    console.log("yay");
+                } else {
+                    console.log("ERROR: status " + xhttp.status);
                 }
-            });
-            xhr.open('POST', 'http://localhost:3000/users/record',true);
-            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            if(dTmp.getTime() - startTime > 1000) {
-                var form = 'url=' + currUrl + '&startTime=' + (startTime/1000).toString() + '&endTime=' + (dTmp.getTime()/1000).toString();
-                xhr.send(form);
             }
-            prevUrl = currUrl;
-            currUrl = url;
-            startTime = dTmp.getTime();
-            delay = 0;
-        }
-        else if (changeInfo.status != "complete") {
-            delay = dTmp.getTime() - startTime;
-        }
+        });
+        xhr.open('POST', 'http://localhost:3000/users/record',true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        var dTmp = new Date();
+        var form = 'url=' + extractDomain(tab.url) + '&startTime=' + dTmp.toString();
+        xhr.send(form);
     }
 });
-
-
