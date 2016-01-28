96dfe400f38ffbb93683b7da11412dded0246104
Danny Thomson <danthom@stanford.edu>
2016-01-28 00:23:55 -0800

    adding redirecting on onUpdated
    Added the code that check that the user was on a restricted website,
    used all the available remaining time, and need to add code to redirect
    page.

--- redirect/background.js
+++ redirect/background.js
@@ -5,31 +5,96 @@
 // });
 
 var currSite;
+var currRestriction;
+var currTimeoutId;
+var currTime = new Date();
+var defaultSite = false;
+
+
+
+ // datepart: 'y', 'm', 'w', 'd', 'h', 'n', 's'
+Date.dateDiff = function(fromdate, todate) {  
+  var diff = todate - fromdate;
+  return [Math.floor(diff/3600000),Math.floor(diff/60000),Math.floor(diff/1000)];
+}
+
+var redirctURL = "http://www.stanford.edu/"
+
+//TODO
+var domainComparison = function() {};
 
 function RestrictedList(category, sites, remainingTime, totalAllowedTime) {
   this.category = category;
   this.sites = sites;
   this.remainingTime = remainingTime;
   this.totalAllowedTime = totalAllowedTime
+  this.checkSite = function(siteToCheck) {
+    for (var j = 0; j < sites.length; j++) {
+      if (siteToCheck.indexOf(sites[j]) != -1) {
+        return true;
+      }
+    }
+    return false
+  };
+
+  //TODO Fix example 0,1,2 - 0,0,5
+  this.usedTime = function(usedTimeArr) {
+    for (var i = 0; i < remainingTime.length; i++) {
+      this.remainingTime[i] = this.remainingTime[i] - usedTimeArr[i]
+    }
+    console.log(this.remainingTime)
+  };
+
+  this.remainingTimeToTime = function() {
+    return (60 * 60 * this.remainingTime[0] + 60 * this.remainingTime[1] + this.remainingTime[2])*1000
+  }
 }
 
-var socialRestrictions = new RestrictedList("Social Media",["http://www.facebook.com",
-  "http://www.twitter.com"],[0,0,15],[0,0,30])
+//TODO add code to clean urls
+var socialRestrictions = new RestrictedList("Social Media",["facebook.com",
+  "twitter.com"],[0,0,15],[0,0,30])
 
-var deadline;
-var defaultSite = false;
+var restrictedSitesArr = [socialRestrictions]
 
+
+var checkRestrictedSite = function(tabId, changeInfo, tab) {
+  //TODO add more complex URL checking.
+  if (currSite == tab.url) return "Same";
+  console.log(tab.url)
+  if(currRestriction != null) {
+    window.clearTimeout(currTimeoutId)
+    console.log(Date.dateDiff(currTime, new Date()))
+    socialRestrictions.usedTime(Date.dateDiff(currTime, new Date()))
+  }
+
+  currSite = tab.url;
+  currRestriction = null;
+  for (var i = 0; i < restrictedSitesArr.length; i++) {
+    if(restrictedSitesArr[i].checkSite(currSite)) {
+      currRestriction = restrictedSitesArr[i];
+    }
+  }
+  if (currRestriction == null) return;
+
+  //set timeout and store id
+  currTime = new Date()
+  console.log(currRestriction.remainingTime)
+  currTimeoutId = window.setTimeout(function() {
+    console.log("BOOM! F@ck!")
+
+  }, currRestriction.remainingTimeToTime());
+}
+
+chrome.tabs.onActivated.addListener(function(tabId, changeInfo, tab) {
+});
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-
-  deadline = new Date(Date.parse(new Date()) + 60 * 1000);
-  console.log(deadline);
-  console.log(tab.url);
-  if(tab.url == "http://www.stanford.edu/") {
+  if(tab.url == redirctURL) {
   	defaultSite = true;
+    return;
   }
-  else {
-  	defaultSite = false;
-  }
+  checkRestrictedSite(tabId, changeInfo, tab)
+  defaultSite = false;
+
 });
 
 
