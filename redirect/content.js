39a344980acc144dfa7bdee1b524fd11548ead8f
Jeff Garnier <jeff1731@stanford.edu>
2016-01-27 21:09:11 -0800

    Receive message in content script from background

--- redirect/content.js
+++ redirect/content.js
@@ -3,3 +3,17 @@ newdiv.setAttribute('id','DisciPlanDiv');
 var timeOpened = new Date();
 newdiv.innerHTML = timeOpened.toString();
 document.body.appendChild(newdiv);
+
+
+
+chrome.runtime.sendMessage({time: "remaining"}, function(response) {
+    console.log(response.time);
+    alert("rec message...");
+    // if(response.time){
+    //   deadline = new Date(response.time);
+    //   timeinterval = setInterval(updateClock, 1000);
+    // }
+    // if(response.defaultSite){
+    //   console.log("Already redirected...")
+    // }
+  });
