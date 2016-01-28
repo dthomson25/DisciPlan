90134d7a0da6cc567e7241b27e052f6a26a056d4
jamesross <jross3@stanford.edu>
2016-01-27 23:33:43 -0800

    removed duplicate callback function code.

--- chromeExtension/background.js
+++ chromeExtension/background.js
@@ -20,44 +20,35 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 });
 
 
+function sendXHR(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener('readystatechange', function(evt) {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                console.log("yay");
+            }
+            else {
+                console.log("ERROR: status " + xhr.status);
+            }
+        }
+    });
+    xhr.open('POST', 'http://localhost:3000/users/record',true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    var dTmp = new Date();
+    var form = 'url=' + extractDomain(url) + '&startTime=' + dTmp.toString();
+    xhr.send(form);
+}
+
+
 chrome.tabs.onActivated.addListener(function(activeInfo) {
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-        var xhr = new XMLHttpRequest();
-        xhr.addEventListener('readystatechange', function(evt) {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    console.log("yay");
-                }
-                else {
-                    console.log("ERROR: status " + xhr.status);
-                }
-            }
-        });
-        xhr.open('POST', 'http://localhost:3000/users/record',true);
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        var dTmp = new Date();
-        var form = 'url=' + extractDomain(tabs[0].url) + '&startTime=' + dTmp.toString();
-        xhr.send(form);
+        sendXHR(tabs[0].url);
     });
 });
 
 
 chrome.tabs.onUpdated.addListener(function(tabid, changeInfo, tab) {
     if (tab.url !== undefined && changeInfo.status == "complete") {
-        var xhr = new XMLHttpRequest();
-        xhr.addEventListener('readystatechange', function (evt) {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    console.log("yay");
-                } else {
-                    console.log("ERROR: status " + xhttp.status);
-                }
-            }
-        });
-        xhr.open('POST', 'http://localhost:3000/users/record',true);
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        var dTmp = new Date();
-        var form = 'url=' + extractDomain(tab.url) + '&startTime=' + dTmp.toString();
-        xhr.send(form);
+        sendXHR(tab.url);
     }
 });
