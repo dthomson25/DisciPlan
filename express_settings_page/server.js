f8b8d5d98dbfda862d464eb53b18c3a21191dfaa
Jeff Garnier <jeff1731@stanford.edu>
2016-02-01 20:39:37 -0800

    Simple express node.js app
    Simple node.js sever that uses express to serve a simple web page.

---
+++ express_settings_page/server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.set('view engine', 'jade');
+app.set('views', './views');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', function (req, res) {
+	console.log('Get to /');
+	res.render('index', {title: 'DisciPlan Settings', message: 'Your settings will be here I promise!'});
+});
+
+app.get('/user_settings', function(req, res) {
+	res.send('Got a get request at /user_settings');
+});
+
+
+app.listen(3000, function () {
+	console.log('Example app listening on port 3000!');
+});
