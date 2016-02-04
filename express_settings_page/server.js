a65c76aa761d8befb8796c4ab6eb0f35dce05a4d
Jeff Garnier <jeff1731@stanford.edu>
2016-02-03 21:24:58 -0800

    Simple nav bar at top

--- express_settings_page/server.js
+++ express_settings_page/server.js
@@ -10,25 +10,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
 	console.log('Get to /');
-	res.send('Home page.');
+	res.render('navbar_fixed_top', {current: '/'});
 
 });
 
-app.get('/user_settings', function(req, res) {
-	console.log('Get to /user_settings');
+app.get('/settings', function(req, res) {
+	console.log('Get to /settings');
 
 	categories = [{ name: 'Social', time: 20}, { name: 'Sports', time: 20 }, { name: 'Entertainment', time: 20}];
 	urls = [[{ name: 'facebook.com' }, { name: 'twitter.com'}],
 	        [{ name: 'espn.com' }, { name: 'sportscenter.com'}],
 	        [{ name: 'cnn.com' }, { name: 'usatoday.com'}]];
 
-	res.render('index', {title: 'DisciPlan Settings', 
-						 message: 'This is your settings page!',
-						 categories: categories, 
-						 url_lists: urls,
-						 urls: [{ name: 'facebook.com' }, { name: 'twitter.com'}]
-						});
+	res.render('settings', {title: 'DisciPlan Settings', 
+						    message: 'This is your settings page!',
+						    categories: categories, 
+						    url_lists: urls,
+						    urls: [{ name: 'facebook.com' }, { name: 'twitter.com'},],
+						    current: '/settings'
+						   });
+	
 });
+
+app.get('/usage', function(req, res) {
+	res.render('usage', {message: 'This will be graphs.',
+		                 current: '/usage'
+		                });
+});
+
+
 
 
 app.listen(3000, function () {
