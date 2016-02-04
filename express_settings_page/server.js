e5911562d9852ecc60208cd895a0f4c38178c887
Jeff Garnier <jeff1731@stanford.edu>
2016-02-03 17:54:01 -0800

    Added a javascript file to the page
    Simple onbutton click calls button() function in edit.js. This
    javascript file will be used to edit the settings page.

--- express_settings_page/server.js
+++ express_settings_page/server.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function (req, res) {
 	console.log('Get to /');
 
-	categories = [{ name: 'Social' }, { name: 'Sports' }, { name: 'Entertainment'}];
+	categories = [{ name: 'Social', time: 20}, { name: 'Sports', time: 20 }, { name: 'Entertainment', time: 20}];
 	urls = [[{ name: 'facebook.com' }, { name: 'twitter.com'}],
 	        [{ name: 'espn.com' }, { name: 'sportscenter.com'}],
 	        [{ name: 'cnn.com' }, { name: 'usatoday.com'}]];
