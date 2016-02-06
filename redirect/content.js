8c185ad8c0dea993ab95bdb8ebcb08a1229323dd
Danny Thomson <danthom@stanford.edu>
2016-02-06 15:30:51 -0800

    changed css from visibility to display for content script

--- redirect/content.js
+++ redirect/content.js
@@ -1,6 +1,6 @@
 var newdiv = document.createElement('div'); 
 newdiv.setAttribute('id','DisciPlanDiv');
-newdiv.setAttribute('style', 'visibility: hidden');
+newdiv.setAttribute('style', 'display: none');
 var timeOpened = new Date();
 newdiv.innerHTML = timeOpened.toString();
 document.body.appendChild(newdiv);
